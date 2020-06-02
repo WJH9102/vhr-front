1a4cd5e7946f2d7f28eaac215808fc074a6150b6
wjh <1602526012@qq.com>
2020-06-02 22:37:16 +0800

    添加vuex存储菜单信息

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+import store from "./store/index";
 import ElementUI from "element-ui";
 import "element-ui/lib/theme-chalk/index.css";
 import {
@@ -23,5 +24,6 @@ Vue.use(ElementUI);
 
 new Vue({
   router,
+  store,
   render: h => h(App)
 }).$mount("#app");
